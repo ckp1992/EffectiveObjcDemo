f4e4a3c603d5c15c9c09417565dc27272c0f066e
kunpengchen <674027158@qq.com>
2016-01-07 23:42:22 +0800

    修改目录结构

---
+++ EffectiveObjcDemo/Chapter 1/Item1/EOCItem1.h
@@ -0,0 +1,11 @@
+//
+//  EOCItem1.h
+//  EffectiveObjcDemo
+//
+//  Created by Cooper on 1/7/16.
+//  Copyright © 2016 Cooper. All rights reserved.
+//
+
+//1.建议熟悉C语言与Objc的语法
+//2.了解指针类型与值类型。
+//3.了解C语言的核心知识，对了解内存模型和指针有帮助
